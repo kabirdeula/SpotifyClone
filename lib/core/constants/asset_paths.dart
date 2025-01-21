b69a8487d4a1b3a3eff66fe57b76150966fba49c
Kabir Deula <kabirdeula167@gmail.com>
2025-01-21 12:49:33 +0545

    feat: 🎨Add asset paths for images and update pubspec.yaml to include image assets

---
+++ lib/core/constants/asset_paths.dart
@@ -0,0 +1,5 @@
+class AssetPaths {
+  static const String basePath = "assets/images";
+  static const String onboardingBackground = "$basePath/onboarding_bg.jpg";
+  static const String branding = "$basePath/branding.png";
+}
