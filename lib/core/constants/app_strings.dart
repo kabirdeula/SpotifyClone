48619a26578f8936d03e5aea511259f8febce6ce
Kabir Deula <kabirdeula167@gmail.com>
2025-01-21 12:49:51 +0545

    feat: 🎨Add AppStrings class for constant strings and update constants export

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,4 @@
+class AppStrings {
+  static const fillerText =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ";
+}
