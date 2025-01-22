64f247e75fef692a99cc1e7ae671cd4c658861c4
Kabir Deula <kabirdeula167@gmail.com>
2025-01-22 15:41:24 +0545

    feat: ✨Add welcome image asset path for the welcome screen

--- lib/core/constants/asset_paths.dart
+++ lib/core/constants/asset_paths.dart
@@ -3,4 +3,5 @@ class AssetPaths {
   static const String onboardingBackground = "$basePath/onboarding_bg.jpg";
   static const String branding = "$basePath/branding.png";
   static const String themePreferenceBackground = "$basePath/theme_preference_bg.jpg";
+  static const String welcome = "$basePath/welcome.png";
 }
