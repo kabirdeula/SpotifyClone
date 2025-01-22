683f7c61404724a6d0c24c2b1ff4337f575aadcf
Kabir Deula <kabirdeula167@gmail.com>
2025-01-22 16:24:41 +0545

    fix: 🐛Change ThemeCubit registration from factory to singleton for improved dependency management

--- lib/core/dependency_injection/cubit_injection.dart
+++ lib/core/dependency_injection/cubit_injection.dart
@@ -3,5 +3,5 @@ import 'package:spotify_clone/features/theme_preference/theme_preference.dart';
 import 'dependency_injection.dart';
 
 void setupCubitInjection() {
-  sl.registerFactory<ThemeCubit>(() => ThemeCubit());
+  sl.registerSingleton<ThemeCubit>(ThemeCubit());
 }
