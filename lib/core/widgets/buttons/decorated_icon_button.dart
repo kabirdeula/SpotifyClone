de79b5dc3601b1b96fcabec819dc76ee66a9bfb7
Kabir Deula <kabirdeula167@gmail.com>
2025-01-22 16:04:47 +0545

    feat: ✨Add text parameter to DecoratedIconButton for improved accessibility

--- lib/core/widgets/buttons/decorated_icon_button.dart
+++ lib/core/widgets/buttons/decorated_icon_button.dart
@@ -2,21 +2,25 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:spotify_clone/core/constants/constants.dart';
+import 'package:spotify_clone/core/widgets/app_text.dart';
 
 class DecoratedIconButton extends StatelessWidget {
   final IconData icon;
   final VoidCallback onPressed;
+  final String text;
 
   const DecoratedIconButton({
     super.key,
     required this.icon,
     required this.onPressed,
+    required this.text,
   });
 
   factory DecoratedIconButton.lightMode({required VoidCallback onPressed}) {
     return DecoratedIconButton(
       icon: Icons.light_mode_outlined,
       onPressed: onPressed,
+      text: 'Light Mode',
     );
   }
 
@@ -24,26 +28,32 @@ class DecoratedIconButton extends StatelessWidget {
     return DecoratedIconButton(
       icon: Icons.dark_mode_outlined,
       onPressed: onPressed,
+      text: 'Dark Mode',
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return ClipOval(
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            color: AppColors.semiTransparent,
-            shape: BoxShape.circle,
-          ),
-          child: IconButton(
-            onPressed: onPressed,
-            icon: Icon(icon, color: Colors.white, size: 30),
+    return Column(
+      children: [
+        ClipOval(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: AppColors.semiTransparent,
+                shape: BoxShape.circle,
+              ),
+              child: IconButton(
+                onPressed: onPressed,
+                icon: Icon(icon, color: Colors.white, size: 30),
+              ),
+            ),
           ),
         ),
-      ),
+        AppText(text: text),
+      ],
     );
   }
 }
