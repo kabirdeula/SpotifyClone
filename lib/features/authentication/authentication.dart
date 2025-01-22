7ded1ce26642151120ac01d5640136bbb0c75317
Kabir Deula <kabirdeula167@gmail.com>
2025-01-22 16:41:22 +0545

    refactor: 🔧Update export paths for authentication screens to a unified presentation directory

--- lib/features/authentication/authentication.dart
+++ lib/features/authentication/authentication.dart
@@ -1,3 +1,3 @@
-export './login/presentation/screens/login_screen.dart';
-export './sign_up/presentation/screens/sign_up_screen.dart';
-export './welcome/presentation/screens/welcome_screen.dart';
+export './presentation/screens/login_screen.dart';
+export './presentation/screens/sign_up_screen.dart';
+export './presentation/screens/welcome_screen.dart';
