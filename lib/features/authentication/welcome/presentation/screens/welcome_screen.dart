1db4d2d36fcc991cfa2bcd68a72372ee894e1744
Kabir Deula <kabirdeula167@gmail.com>
2025-01-22 14:45:38 +0545

    feat: ✨Implement welcome screen layout with branding and action buttons

--- lib/features/authentication/welcome/presentation/screens/welcome_screen.dart
+++ lib/features/authentication/welcome/presentation/screens/welcome_screen.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_clone/core/constants/constants.dart';
+import 'package:spotify_clone/core/widgets/widgets.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+          child: Column(
+            spacing: 24,
+            children: [
+              brandingImage(),
+              AppText(
+                text: 'Enjoy Listening To Music',
+                color: Colors.black,
+                appTextStyle: AppTextStyle.subtitle,
+              ),
+              AppText(
+                text:
+                    'Spotify is a proprietary Swedish audio streaming and media services provider',
+                color: Colors.grey,
+                appTextStyle: AppTextStyle.body,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                spacing: 12,
+                children: [
+                  Expanded(
+                    child: CustomElevatedButton(
+                      onPressed: () {},
+                      label: 'Register',
+                    ),
+                  ),
+                  Expanded(
+                    child: CustomElevatedButton(
+                      onPressed: () {},
+                      label: 'Sign in',
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.label,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
