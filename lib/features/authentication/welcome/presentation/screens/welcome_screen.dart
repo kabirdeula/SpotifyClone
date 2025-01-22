e1322a9477c91144f468b5428f0a60b249a5a86e
Kabir Deula <kabirdeula167@gmail.com>
2025-01-22 15:42:02 +0545

    refactor: 🦄Refactor welcome screen layout to use Stack for improved design and positioning

--- lib/features/authentication/welcome/presentation/screens/welcome_screen.dart
+++ lib/features/authentication/welcome/presentation/screens/welcome_screen.dart
@@ -8,48 +8,60 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
-          child: Column(
-            spacing: 24,
-            children: [
-              brandingImage(),
-              AppText(
-                text: 'Enjoy Listening To Music',
-                color: Colors.black,
-                appTextStyle: AppTextStyle.subtitle,
-              ),
-              AppText(
-                text:
-                    'Spotify is a proprietary Swedish audio streaming and media services provider',
-                color: Colors.grey,
-                appTextStyle: AppTextStyle.body,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                spacing: 12,
-                children: [
-                  Expanded(
-                    child: CustomElevatedButton(
-                      onPressed: () {},
-                      label: 'Register',
-                    ),
-                  ),
-                  Expanded(
-                    child: CustomElevatedButton(
-                      onPressed: () {},
-                      label: 'Sign in',
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: AppColors.label,
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Image.asset(
+              AssetPaths.welcome,
+              fit: BoxFit.cover,
+              width: MediaQuery.sizeOf(context).width / 2.7,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+            child: Column(
+              spacing: 24,
+              children: [
+                Spacer(),
+                brandingImage(),
+                AppText(
+                  text: 'Enjoy Listening To Music',
+                  color: Colors.black,
+                  appTextStyle: AppTextStyle.subtitle,
+                ),
+                AppText(
+                  text:
+                      'Spotify is a proprietary Swedish audio streaming and media services provider',
+                  color: Colors.grey,
+                  appTextStyle: AppTextStyle.body,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  spacing: 12,
+                  children: [
+                    Expanded(
+                      child: CustomElevatedButton(
+                        onPressed: () {},
+                        label: 'Register',
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    Expanded(
+                      child: CustomElevatedButton(
+                        onPressed: () {},
+                        label: 'Sign in',
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.label,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
