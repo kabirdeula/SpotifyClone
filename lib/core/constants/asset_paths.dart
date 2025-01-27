d4a7ed61cf83b8a9d08f1bef0ab533982ad12f52
Kabir Deula <kabirdeula167@gmail.com>
2025-01-27 15:00:10 +0545

    feat: ✨Add asset paths for Apple and Google images to enhance branding options

--- lib/core/constants/asset_paths.dart
+++ lib/core/constants/asset_paths.dart
@@ -1,7 +1,10 @@
 class AssetPaths {
   static const String basePath = "assets/images";
-  static const String onboardingBackground = "$basePath/onboarding_bg.jpg";
+
+  static const String apple = "$basePath/apple.png";
   static const String branding = "$basePath/branding.png";
+  static const String google = "$basePath/google.png";
+  static const String onboardingBackground = "$basePath/onboarding_bg.jpg";
   static const String themePreferenceBackground = "$basePath/theme_preference_bg.jpg";
   static const String welcome = "$basePath/welcome.png";
 }
