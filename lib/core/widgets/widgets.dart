e19bcccc9372c5e9220e3ff54a351831178a470d
Kabir Deula <kabirdeula167@gmail.com>
2025-01-27 14:23:00 +0545

    feat: ✨Add customizable TextFormField widget for consistent styling and functionality

--- lib/core/widgets/widgets.dart
+++ lib/core/widgets/widgets.dart
@@ -1,5 +1,6 @@
 export './app_text.dart';
 export './buttons/custom_elevated_button.dart';
 export './buttons/decorated_icon_button.dart';
+export './custom_text_form_field.dart';
 export './shared_widgets.dart';
 export './stack_screen_layout.dart';
