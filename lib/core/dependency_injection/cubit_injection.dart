aabfaeca439102df1d9c79f0a18bb9a0aaca4737
Kabir Deula <kabirdeula167@gmail.com>
2025-02-05 14:55:38 +0545

    feat: ✨Add AuthCubit and AuthHiveService to dependency injection setup

--- lib/core/dependency_injection/cubit_injection.dart
+++ lib/core/dependency_injection/cubit_injection.dart
@@ -1,7 +1,9 @@
+import 'package:spotify_clone/features/authentication/authentication.dart';
 import 'package:spotify_clone/features/theme_preference/theme_preference.dart';
 
 import 'dependency_injection.dart';
 
 void setupCubitInjection() {
   sl.registerSingleton<ThemeCubit>(ThemeCubit());
+  sl.registerSingleton<AuthCubit>(AuthCubit());
 }
